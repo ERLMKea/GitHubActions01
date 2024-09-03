7417124f033a3a2aa5dbbe90649df61f58a65409
erlm <erlm@kea.dk>
2024-09-03 22:00:50 +0200

    github actions

---
+++ src/main/java/org/example/githubactions01/GitHubActions01Application.java
@@ -0,0 +1,13 @@
+package org.example.githubactions01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubActions01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitHubActions01Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/githubactions01/GitHubActions01ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.githubactions01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubActions01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
